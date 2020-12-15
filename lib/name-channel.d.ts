3759e1625f0c591ab5f51d3b90771ceabe7b2ae0
Bonflintstone <freddy@railslove.com>
2020-12-15 11:29:01 +0100

    Adapt to how npm packages should look like

--- lib/name-channel.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const nameChannel: (options: {
-    branchName: string;
-}) => void;
--- lib/notification.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const sendNotification: (options: {
-    channel: string;
-    prNumber: string;
-    sha: string;
-    token: string;
-    expoName: string;
-    githubRepo: string;
-}) => void;
